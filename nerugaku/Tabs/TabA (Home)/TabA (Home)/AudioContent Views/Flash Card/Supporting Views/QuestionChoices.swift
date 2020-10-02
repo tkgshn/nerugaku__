8b573d762f82a6cdc1d4a1c864faad8f6654f4b6
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2020-10-03 05:47:21 +0900

    大規模な補修をするのでその前にコミット

--- nerugaku/Tabs/TabA (Home)/TabA (Home)/AudioContent Views/Flash Card/Supporting Views/QuestionChoices.swift
+++ nerugaku/Tabs/TabA (Home)/TabA (Home)/AudioContent Views/Flash Card/Supporting Views/QuestionChoices.swift
@@ -9,31 +9,6 @@
 import SwiftUI
 
 // MARK: - 回答の選択肢を生成
-//表示中の単語を除く同じシーンの中からランダムで4つ表示（これが選択肢になる）（日本語を表示）
-//struct ChoiceGeneration: View {
-//    var audioContent: AudioContent
-//
-//    func addDic() {
-//        var dic = ForEach(1..<audioContent.allpharase+1) { num in
-//            self.audioContent.phrases[String(num)]!.japanese
-//        }
-//    }
-//
-//    var body: some View {
-//        VStack {
-//
-//            Text(audioContent.)
-//        }
-//    }
-//}
-//
-//struct QuestionChoices_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ChoiceGeneration(audioContent: audioContentData[0])
-//            .previewLayout(.sizeThatFits)
-//    }
-//}
-
 
 // MARK: - BoxView
 //選択肢は同じid内のphrasesをランダムに取得したものにする
@@ -41,14 +16,58 @@ struct BoxView: View {
     @Binding var selectedBox: BoxType
     let color: Color
     let boxType: BoxType
-    //    フレーズを選択肢として辞書に入れる
-//        同じaudioContent内のJapaneseを選択肢として辞書にインポート
-//    var audioContent: AudioContent
+    //        同じaudioContent内のJapaneseを選択肢として辞書にインポート
+    //    var audioContent: AudioContent
     
-    let choiceDic = ["example text A", "example text B", "example text c", "TestD"]
+    //  辞書を作成
+    let Dic = [
+        ["japanese": "私", "english": "I", "phraseAudioName"  : "人称"],
+        ["japanese": "私とあなた", "english": "I and You", "phraseAudioName"  : "国と言語"],
+        ["japanese": "私たち", "english": "both of us", "phraseAudioName"  : "人称"],
+        ["japanese": "彼", "english": "he", "phraseAudioName"  : "国と言語"],
+        ["japanese": "彼と彼女", "english": "he and she", "phraseAudioName"  : "人称"],
+        ["japanese": "彼ら", "english": "they both", "phraseAudioName"  : "国と言語"],
+    ]
+    
+    //    {
+    //        // まずは辞書からjapaneseだけを抽出
+    //        DicArry = Dic.map(){$0["japanese"]!} // この時点の中身 ["私", "私とあなた", "私たち", "彼", "彼と彼女", "彼ら"]
+    //
+    //        // DicArryをシャッフルする
+    //        ShuffledDicArry = DicArry.shuffled() // シャッフルの位置は毎回変化するが、["彼ら", "私たち", "彼と彼女", "彼", "私とあなた", "私"]のようになる
+    //
+    //        // 3つだけ取得する
+    //        // この際は辞書のindexが被っていないので、同じものが選択肢になることはない
+    //
+    //        //        print("①　\(ShuffledDicArry[0])")
+    //        //        print("②　\(ShuffledDicArry[1])")
+    //        //        print("③　\(ShuffledDicArry[2])")
+    //    }
+    
+    
     var body: some View {
         //        要素の中からランダムに取得
-        Text(choiceDic.randomElement()!)
+        
+        
+        
+        //        Text(ShuffledDicArry[0])
+        
+        
+        //            Button("tap") {
+        //                DicArry = self.Dic.map(){$0["japanese"]!}
+        //                ShuffledDicArry = self.DicArry.shuffled()
+        //            }
+        //        ForEach (0..<6) { n in
+        //            let japanese = [(phrases[n]["japanese"])!]
+        //            let japaneseText = ("\(japanese.randomElement()!)")
+        //
+        //        }
+        //        Text(choiceDic.map{$0["japanese"]!})
+        Text("test")
+            //        ForEach (0..<6) { n in
+            //            Text(choiceDic[0]["japanese"]!)
+            //        }
+            //        上記の辞書の中から、ランダムで3つほど選択する
             .font(.headline)
             .padding()
             .background(color)
@@ -59,8 +78,11 @@ struct BoxView: View {
             .padding()
             .border(Color.black, width: selectedBox == boxType ? 4 : 0)
     }
-    
 }
+
+
+
+
 
 
 enum BoxType: String {
@@ -88,6 +110,7 @@ struct SingleSelectableBoxView: View {
     @Binding var selectedBox: BoxType
     var body: some View {
         VStack {
+            
             BoxView(selectedBox: $selectedBox, color: .red, boxType: .red)
             BoxView(selectedBox: $selectedBox, color: .green, boxType: .green)
             BoxView(selectedBox: $selectedBox, color: .blue, boxType: .blue)
@@ -96,3 +119,12 @@ struct SingleSelectableBoxView: View {
     }
 }
 
+
+
+// MARK: - Preview
+struct QuestionRootView_Previews2: PreviewProvider {
+    static var previews: some View {
+        QuestionRootView(audioContent: audioContentData[0])
+        
+    }
+}
